41a7cc5f280ce148fce682a407d123038da5c068
TheMeano <123184115+TheMeano@users.noreply.github.com>
2023-09-21 17:28:39 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header(){
+
+    setInterval(updateTime, 1000);
+
+    const [currentTime, setTime] = useState(new Date().toLocaleTimeString());
+
+    function updateTime(){
+        var newTime = new Date().toLocaleTimeString();
+
+        setTime(newTime);
+    }
+
     return(
         <header>
-            <h1>Keeper</h1>
+            <h1 style={{float : "left"}}>Keeper second II</h1>
+            <h1 style={{float : "right"}}>{currentTime}</h1>
         </header>
-    );
-}
+    )
+};
 
 export default Header;
